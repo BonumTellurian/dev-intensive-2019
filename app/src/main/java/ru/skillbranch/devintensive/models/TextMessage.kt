0f9b6e13b161215329d05dc0873615ff49c7e4ad
BonumTellurian <moshushi@gmail.com>
2019-07-04 22:35:38 +0300

    try1 Base Message

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -1,20 +1,20 @@
 package ru.skillbranch.devintensive.models
 
-//import ru.skillbranch.devintensive.extensions.humanizeDiff
+import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
-//class TextMessage(
-//    id:String,
-//    from:User?,
-//    chat: Chat,
-//    isIncoming : Boolean = false,
-//    date:Date = Date(),
-//    var text:String?
-//) : BaseMessage(id,from, chat, isIncoming, date) {
-//
-////}
-//////        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    override fun formatMesage(): String = "id:$id ${from?.firstName}" +
-////             " ${if(isIncoming) "получил" else "отправил" } сообщение \"$text\" ${date.humanizeDiff()}"
-//             " ${if(isIncoming) "получил" else "отправил" } сообщение \"$text\" ${date.()}"
+class TextMessage(
+    id:String,
+    from:User?,
+    chat: Chat,
+    isIncoming : Boolean = false,
+    date:Date = Date(),
+    var text:String?
+) : BaseMessage(id,from, chat, isIncoming, date) {
+
 //}
+////        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun formatMesage(): String = "id:$id ${from?.firstName}" +
+             " ${if(isIncoming) "получил" else "отправил" } сообщение \"$text\" ${date.humanizeDiff()}"
+             " ${if(isIncoming) "получил" else "отправил" } сообщение \"$text\" ${date.()}"
+}
