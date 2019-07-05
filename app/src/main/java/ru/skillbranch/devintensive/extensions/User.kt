a7124a13b8db8c2e500dbf3e989065d4dec6fd54
BonumTellurian <moshushi@gmail.com>
2019-07-05 21:47:58 +0300

    try4 Base Message

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -1,24 +1,39 @@
 package ru.skillbranch.devintensive.extensions
 
-//import ru.skillbranch.devintensive.Utils.Utils
+import ru.skillbranch.devintensive.Utils.Utils
+import ru.skillbranch.devintensive.Utils.Utils.parseFullName
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.models.UserView
-//import ru.skillbranch.devintensive.extensions.Date
-import java.util.*
 
-//fun User.toUserView() : UserView {
-//
+//import ru.skillbranch.devintensive.models.User
+//import ru.skillbranch.devintensive.models.UserView
+
+fun User.toUserView() : UserView {
+
 //    val nickName = Utils.transliteration("$firstName $lastName")
-//    val initials = Utils.toInitials(firstName lastName)
-//    val status = if(lastVisit == null) "Ещё ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit.humanizeDiff()}"
-//
-//    return UserView(
-//        id,
-//        fullName = "$firstName $lastName",
-//
-//        avatar = avatar,
-//        nickName = nickName,
-//        initials = initials,
-//        status = )
-//}
+//    val nickName = Utils.transliteration("$firstName $lastName")
+//    val nickName = Utils.transliteration("$firstName $lastName")
+//    val initials = Utils.toInitials("$fullName")
+//    val initials = Utils.toInitials("$firstName $lastName")
+//    val initials = Utils.toInitials("$firstName ${this.lastName}")
+
+//    val nickName = Utils.transliteration("$firstName $lastName")
+//    val initials = Utils.toInitials("$firstName $lastName")
+
+//    val firstName = parseFullName(fullName)
+
+    val nickName = Utils.transliteration("${this.firstName} ${this.lastName}")
+    val initials = Utils.toInitials("${this.firstName} ${this.lastName}")
+
+    val status = if(lastVisit == null) "Ещё ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit.humanizeDiff()}"
+
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+
+        avatar = avatar,
+        nickName = nickName,
+        initials = initials,
+        status = status)
+}
 
