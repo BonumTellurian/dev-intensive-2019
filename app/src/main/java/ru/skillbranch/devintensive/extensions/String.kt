53154a4454a19b5aebe58e13245156514caaa56c
BonumTellurian <moshushi@gmail.com>
2019-07-09 17:53:12 +0300

    try 3 stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -7,21 +7,32 @@ fun String.truncate(cutter:Int = 16):String?{
 
 }
 
-fun String.stripHtml():String?{
+fun String.stripHtml():String{
     val String = this
 //    val matchResult = """\d+"""
 //    val matchResult = """<\s*a[^>]*>(.*?)<\s*/\s*a>"""
     val matchResult = """<.*?>"""
-    val mR2 = """\&amp;"""
-    val mR3 = """\&lt;"""
-    val mR4 = """\&gt;"""
+//    val mR2 = """\&amp;"""
+//    val mR3 = """\&lt;"""
+//    val mR4 = """\&gt;"""
     val mR5 = """\&prime;"""
     val mR6 = """\&Prime;"""
     val mR7 = """\&quot;"""
-    val mR8 = """\\s+"""
-    return String.replace(matchResult.toRegex(), "").replace(mR2.toRegex(),"").
-        replace(mR3.toRegex(), "").replace(mR4.toRegex(), "").
-        replace(mR5.toRegex(), "").replace(mR6.toRegex(), "").
-        replace(mR7.toRegex(), "").replace(mR8.toRegex(), " ")
+//    val mR8 = """\\s+"""
+    val mR8 = """  +"""
+//    val mR8 = """ {2,}"""
+    return String.replace(matchResult.toRegex(), "")
+//        .replace(mR2.toRegex(),"")
+        .replace("&", "")
+//        .replace(mR3.toRegex(), "")
+        .replace("<", "")
+//        .replace(mR4.toRegex(), "")
+        .replace(">", "")
+
+        .replace(mR5.toRegex(), "")
+        .replace(mR6.toRegex(), "")
+        .replace(mR7.toRegex(), "")
+        .replace(mR8.toRegex(), " ")
+//        .replace(" {2,}", " ")
 
 }
