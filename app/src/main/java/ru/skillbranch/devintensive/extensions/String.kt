649cb63bdd469a00e4d55dab4d82675c47741c99
BonumTellurian <moshushi@gmail.com>
2019-07-09 14:47:55 +0300

    try 1 String.truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -5,6 +5,12 @@ fun String.truncate(cutter:Int = 16):String?{
 
     if (String.length >cutter) return String.substring(0, cutter).trimEnd()+"..." else return this
 
+}
 
-
+fun String.stripHtml():String?{
+    val String = this
+//    val matchResult = """\d+"""
+//    val matchResult = """<\s*a[^>]*>(.*?)<\s*/\s*a>"""
+    val matchResult = """<.*?>"""
+    return String.replace(matchResult.toRegex(), "")
 }
