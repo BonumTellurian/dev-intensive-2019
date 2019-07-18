bc2488f8c7d8fb76a9640ffd53a3e0be14ff7ff1
BonumTellurian <moshushi@gmail.com>
2019-07-18 20:33:49 +0300

    Android 3 lesson test-RUN and task 1 maked
    Can't go throo test 3 check negative question

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -14,12 +14,25 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
 
     fun listenAnswer(answer:String) : Pair<String, Triple<Int, Int, Int>>{
 
+//        return when{
+//            question.answers.contains(answer)
+//            question.answers.contains(answer)
+//        }
+
+//        return if(question.answers.contains(answer) && ! Question.IDLE) {
+//            question = question.nextQuestion()
+//            "Отлично - ты справился\n${question.question}" to status.color
+//        } else {
+//            status = status.nextStatus()
+//            "Это неправильный ответ\n${question.question}" to status.color
+//        }
+
         return if(question.answers.contains(answer)){
             question = question.nextQuestion()
-            "Отлично - ты справился!\n${question.question}" to status.color
+            "Отлично - ты справился\n${question.question}" to status.color
         } else {
             status = status.nextStatus()
-            "Это не правильный ответ!\n${question.question}" to status.color
+            "Это неправильный ответ\n${question.question}" to status.color
         }
 
     }
@@ -28,7 +41,7 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
         NORMAL(Triple(255,255,255)),
         WARNING(Triple(255,120,0)),
         DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0));
+        CRITICAL(Triple(255,0,0));
 
         fun nextStatus():Status{
             return if(this.ordinal < values().lastIndex){
@@ -42,23 +55,78 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
     enum class Question(val question:String, val answers:List<String>){
         NAME("Как меня зовут?", listOf("бендер", "bender")){
             override fun nextQuestion(): Question = PROFESSION
+//            override fun validateAnswer(): Boolean = Regex(pattern = "[A-Z]").matches(input = answers.toString())
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")){
             override fun nextQuestion(): Question = MATERIAL
+//            override fun validateAnswer(): Boolean = Regex(pattern = "[a-z]").matches(input = answers.toString())
+
         },
         MATERIAL("Из чего я сделан?", listOf("метал", "дерево", "metal", "iron", "wood")){
             override fun nextQuestion(): Question = BDAY
+//            override fun validateAnswer(): Boolean = Regex(pattern = """\D""").matches(input = answers.toString())
+
         },
         BDAY("Когда меня создали?", listOf("2993")){
             override fun nextQuestion(): Question = SERIAL
+//            override fun validateAnswer(): Boolean = Regex(pattern = """\d""").matches(input = answers.toString())
+
         },
         SERIAL("Мой серийный номер?", listOf("2716057")){
             override fun nextQuestion(): Question = IDLE
+//            override fun validateAnswer(): Boolean = Regex(pattern = "[0-9]{7}").matches(input = answers.toString())
+
         },
         IDLE("На этом все, вопросов больше нет", listOf()){
             override fun nextQuestion(): Question = IDLE
+//            override fun validateAnswer(): Boolean = true
+
         };
 
         abstract fun nextQuestion():Question
+//        abstract fun validateAnswer(answers: String): Boolean
+
+//        fun validate(question: String) : Boolean{
+//            return when(this){
+//                NAME -> Regex(pattern = "[A-Z]").matches(input = question)
+//                PROFESSION -> Regex(pattern = "[a-z]").matches(input=question)
+//                MATERIAL -> Regex(pattern = """\D""").matches(input=question)
+//                BDAY  -> Regex(pattern = """\d""").matches(input=question)
+//                SERIAL -> Regex(pattern = "[0-9]{7}").matches(input=question)
+//                IDLE -> true
+//            }
+//        }
     }
+
+//    enum class Validate(val question:String, val answers:List<String>){
+//        NAME,
+//        PROFESSION,
+//        MATERIAL,
+//        BDAY,
+//        SERIAL,
+//        IDLE;
+//
+//        fun valid(question: String) : Boolean{
+//            return when(this){
+//                NAME -> Regex(pattern = "[A-Z]").matches(input = question)
+//                PROFESSION -> Regex(pattern = "[a-z]").matches(input=question)
+//                MATERIAL -> Regex(pattern = """\D""").matches(input=question)
+//                BDAY  -> Regex(pattern = """\d""").matches(input=question)
+//                SERIAL -> Regex(pattern = "[0-9]{7}").matches(input=question)
+//                IDLE -> true
+//            }
+//        }
+//
+//        open fun promt() : String {
+//            return when(this){
+//                NAME -> "Имя должно начинаться с заглавной буквы"
+//                PROFESSION -> "Профессия должна начинаться со строчной буквы"
+//                MATERIAL -> "Материал не должен содержать цифр"
+//                BDAY -> "Год моего рождения должен содержать только цифры"
+//                SERIAL -> "Серийный номер содержит только цифры, и их 7"
+//                IDLE -> ""
+//            }
+//        }
+//
+//    }
 }
