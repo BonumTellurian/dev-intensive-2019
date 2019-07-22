823b90b958aefaa18f3a75a1458d00446279994d
BonumTellurian <moshushi@gmail.com>
2019-07-22 22:08:52 +0300

    Android 3 lesson Unit-test Task-1 and Task-2 Try 2

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -12,9 +12,57 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
 
     }
 
-    fun listenAnswer(answer:String) : Pair<String, Triple<Int, Int, Int>> {
+    fun Question.validateAnswer() : Boolean{
+//        fun valid(question: String) : Boolean{
+                        return when(this){
+                Question.NAME -> Regex(pattern = "[A-Z]").matches(input = question)
+                Question.PROFESSION -> Regex(pattern = "[a-z]").matches(input=question)
+                Question.MATERIAL -> Regex(pattern = """\D""").matches(input=question)
+                Question.BDAY  -> Regex(pattern = """\d""").matches(input=question)
+                Question.SERIAL -> Regex(pattern = "[0-9]{7}").matches(input=question)
+                Question.IDLE -> true
+//                            esle -> false
+            }
+        }
 
-        return if(question.answers.contains(answer)){
+
+    fun Question.promt() : String {
+            return when(this){
+                Question.NAME -> "Имя должно начинаться с заглавной буквы"
+                Question.PROFESSION -> "Профессия должна начинаться со строчной буквы"
+                Question.MATERIAL -> "Материал не должен содержать цифр"
+                Question.BDAY -> "Год моего рождения должен содержать только цифры"
+                Question.SERIAL -> "Серийный номер содержит только цифры, и их 7"
+                Question.IDLE -> ""
+            }
+        }
+
+
+
+//    fun listenAnswer(answer:String) : Pair<String, Triple<Int, Int, Int>> {
+//        if (!question.validateAnswer())
+//            return "${question.promt()}\n${question.question}" to status.color
+////            "${question.promt()}" to status.color
+////            "${question.promt()}\n" to status.color
+//        return  if (question.answers.contains(answer)) {
+//                question = question.nextQuestion()
+//                "Отлично - ты справился\n${question.question}" to status.color
+//            } else {
+//                if (status == Status.CRITICAL) {
+//                    status = Status.NORMAL
+//                    question = Question.NAME
+//                    "Это неправильный ответ. Давай все по новой\\n${question.question}" to status.color
+//                } else {
+//                    status = status.nextStatus()
+//                    "Это неправильный ответ\n${question.question}" to status.color
+//                }
+//            }
+//        }
+
+
+// Correct Second!!!
+        fun listenAnswer(answer:String) : Pair<String, Triple<Int, Int, Int>> {
+        return if(question.answers.contains(answer.toLowerCase())){
             question = question.nextQuestion()
             "Отлично - ты справился\n${question.question}" to status.color
         } else {
@@ -29,6 +77,9 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
                 "Это неправильный ответ\n${question.question}" to status.color
             }
         }
+        }
+
+
 
 //        return when (question) {
 //            question.answers.contains(answer) ->
@@ -70,7 +121,7 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
 //            "Это неправильный ответ\n${question.question}" to status.color
 //        }
 
-    }
+//    }
 
     enum class Status(val color : Triple<Int, Int, Int>){
         NORMAL(Triple(255,255,255)),
